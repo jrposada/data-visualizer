031e8b78cbb8e4d59ea568b603e17282984d6d83
Javier Rodriguez Posada <javiropos@gmail.com>
2021-01-30 09:44:17 +0100

    Allow reimport same file

--- src/frontend/app/toolbar/import-file/import-file.component.ts
+++ src/frontend/app/toolbar/import-file/import-file.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
 import { DataService } from "../../services/data/data.service";
 import { EFileType, FileImporterService } from "../../services/file-importer/file-importer.service";
 
@@ -8,15 +9,19 @@ import { EFileType, FileImporterService } from "../../services/file-importer/fil
     styleUrls: ["./import-file.component.scss"]
 })
 export class ImportFileComponent {
+    public inputFile: FormControl = new FormControl();
     public supportedFileTypes = EFileType.Xlsx;
 
-    constructor(private fileImporterService: FileImporterService, private dataService: DataService) { }
+    constructor(
+        private fileImporterService: FileImporterService,
+        private dataService: DataService
+    ) { }
 
     public handleFileInput(files: FileList) {
         this.fileImporterService.importFiles(files)
             .subscribe(
                 dataFrame => this.dataService.setData(dataFrame),
                 err => alert(err)
-            );
+            ).add(this.inputFile.setValue(null, {emitEvent: false}));
     }
 }
